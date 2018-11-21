d2464cbf3d4cd66666b1fab1ffee0269dd9296e0
Zhihan-Lu <zl55@rice.edu>
2018-11-20 22:30:56 -0600

    Updates

---
+++ precache-manifest.028fff1396d821457d24ec389fcaeff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0634e0360f2cdf65bccc",
+    "url": "/static/js/main.0634e036.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "0634e0360f2cdf65bccc",
+    "url": "/static/css/main.3b296a60.chunk.css"
+  },
+  {
+    "revision": "c939565431c1680e4bda8868b8c12137",
+    "url": "/index.html"
+  }
+];
